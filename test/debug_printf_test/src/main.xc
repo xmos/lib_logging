d1653f6724bb3bafc16960fb4bd2ffd2b813b282
David Lacey <david.lacey@xmos.com>
2015-01-14 16:02:18 +0000

    Added test and example

---
+++ test/debug_printf_test/src/main.xc
@@ -0,0 +1,12 @@
+#include <debug_print.h>
+
+int main() {
+  debug_printf("Hello World\n");
+  debug_printf("An int: %d\n", -5);
+  debug_printf("An unsigned int: %u\n", 5);
+  debug_printf("A string: %s\n", "foo");
+  debug_printf("A hexadecimal int: 0x%x\n", 0xabcd);
+  debug_printf("A char: %c\n", 'X');
+
+  return 0;
+}
---
+++ test/runtests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import xmostest
+
+xmostest.init()
+
+xmostest.register_group("lib_logging",
+                        "simple_tests",
+                        "Simple functionality tests",
+"""
+There is currently just one test for this library. It is a simple test that
+uses the various format specifiers and checks the output.
+""")
+
+xmostest.build("debug_printf_test")
+
+resources = xmostest.request_resource("xsim")
+
+tester = xmostest.ComparisonTester(open('test.expect'),
+                                   'lib_logging', 'simple_tests',
+                                   'basic_functionality_test', {})
+
+xmostest.run_on_simulator(resources['xsim'],
+                          'debug_printf_test/bin/debug_printf_test.xe',
+                          tester=tester)
+
+xmostest.finish()
