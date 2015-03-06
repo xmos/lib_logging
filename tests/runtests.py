21f8f6485a5d05628e99afe03342e5ecbb2d5cd0
David Lacey <david.lacey@xmos.com>
2015-03-06 16:05:49 +0000

    Update test to use new xmostest build method

--- tests/runtests.py
+++ tests/runtests.py
@@ -11,8 +11,6 @@ There is currently just one test for this library. It is a simple test that
 uses the various format specifiers and checks the output.
 """)
 
-xmostest.build("debug_printf_test")
-
 resources = xmostest.request_resource("xsim")
 
 tester = xmostest.ComparisonTester(open('test.expect'),
