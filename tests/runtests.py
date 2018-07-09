99888b54c2aebe0d0743ae8c2c6f92934a2310a5
Peter Hedinger <peter@xmos.com>
2018-07-09 10:17:08 +0200

    Re-structure runtests.py in line with every other repo.

--- tests/runtests.py
+++ tests/runtests.py
@@ -2,24 +2,16 @@
 import xmostest
 
 if __name__ == "__main__":
-	xmostest.init()
+    xmostest.init()
 
-	xmostest.register_group("lib_logging",
-	                        "simple_tests",
-	                        "Simple functionality tests",
-	"""
-	There is currently just one test for this library. It is a simple test that
-	uses the various format specifiers and checks the output.
-	""")
+    xmostest.register_group("lib_logging",
+                            "simple_tests",
+                            "Simple functionality tests",
+    """
+    There is currently just one test for this library. It is a simple test that
+    uses the various format specifiers and checks the output.
+    """)
 
-	resources = xmostest.request_resource("xsim")
+    xmostest.runtests()
 
-	tester = xmostest.ComparisonTester(open('test.expect'),
-	                                   'lib_logging', 'simple_tests',
-	                                   'basic_functionality_test', {})
-
-	xmostest.run_on_simulator(resources['xsim'],
-	                          'debug_printf_test/bin/debug_printf_test.xe',
-	                          tester=tester)
-
-	xmostest.finish()
+    xmostest.finish()
